302461eebb2f66fc3fe4eb87753c7471dd6f45cc
Nikola Jovic <jovnikola99@gmail.com>
2025-01-26 17:37:07 +0100

    Spring Boot project initialization

---
+++ src/test/java/uns/ac/rs/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uns.ac.rs.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
