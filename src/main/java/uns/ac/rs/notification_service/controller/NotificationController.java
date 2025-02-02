77c7fc431716b548b4375e77d1a30737e059025a
Ruzica74 <ruzicadosenovicc@gmail.com>
2025-02-02 13:48:06 +0100

    feat: Adding notification management (#6)
    Co-authored-by: Ruzica74 <ruzicaa997@gmail.com>

---
+++ src/main/java/uns/ac/rs/notification_service/controller/NotificationController.java
@@ -0,0 +1,52 @@
+package uns.ac.rs.notification_service.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uns.ac.rs.notification_service.dto.NotificationDTO;
+import uns.ac.rs.notification_service.dto.request.CreateNotificationRequest;
+import uns.ac.rs.notification_service.dto.response.MessageResponse;
+import uns.ac.rs.notification_service.service.NotificationService;
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/notifications")
+public class NotificationController {
+    @Autowired
+    private NotificationService notificationService;
+
+    //endpoint koriste ReservationService i RatingService
+    @PostMapping("/create")
+    public ResponseEntity<?> createNotification(@Valid @RequestBody CreateNotificationRequest createNotificationRequest,
+                                                @RequestHeader("Authorization") String authorizationHeader) {
+        String jwtToken = authorizationHeader.replace("Bearer ", "");
+        MessageResponse messageResponse = notificationService
+                .createNotification(createNotificationRequest, jwtToken);
+        return ResponseEntity.ok(messageResponse);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllMyNotifications(@RequestHeader("Authorization") String authorizationHeader) {
+        String jwtToken = authorizationHeader.replace("Bearer ", "");
+        List<NotificationDTO> notifications = notificationService.getAllMyNotifications(jwtToken);
+        return ResponseEntity.ok(notifications);
+    }
+
+    @PutMapping("/read/{notificationId}")
+    public ResponseEntity<?> readNotification(@PathVariable String notificationId,
+                                              @RequestHeader("Authorization") String authorizationHeader) {
+        String jwtToken = authorizationHeader.replace("Bearer ", "");
+        MessageResponse messageResponse = notificationService.readNotification(notificationId, jwtToken);
+        return ResponseEntity.ok(messageResponse);
+    }
+
+    @DeleteMapping("/delete/{notificationId}")
+    public ResponseEntity<?> deleteNotification(@PathVariable String notificationId,
+                                                @RequestHeader("Authorization") String authorizationHeader) {
+        String jwtToken = authorizationHeader.replace("Bearer ", "");
+        MessageResponse messageResponse = notificationService.deleteNotification(notificationId, jwtToken);
+        return ResponseEntity.ok(messageResponse);
+    }
+}
---
+++ src/main/java/uns/ac/rs/notification_service/service/HostNotificationSettingsService.java
@@ -0,0 +1,94 @@
+package uns.ac.rs.notification_service.service;
+
+import org.springframework.stereotype.Service;
+import uns.ac.rs.notification_service.dto.HostNotificationSettingsDTO;
+import uns.ac.rs.notification_service.dto.client.UserDTO;
+import uns.ac.rs.notification_service.dto.request.UpdateHostNotificationSettingsRequest;
+import uns.ac.rs.notification_service.dto.response.MessageResponse;
+import uns.ac.rs.notification_service.mapper.HostNotificationSettingsMapper;
+import uns.ac.rs.notification_service.model.HostNotificationSettings;
+import uns.ac.rs.notification_service.repository.HostNotificationSettingsRepository;
+import uns.ac.rs.notification_service.service.client.UserServiceClient;
+
+@Service
+public class HostNotificationSettingsService {
+    private final HostNotificationSettingsRepository hostNotificationSettingsRepository;
+
+    private final UserServiceClient userServiceClient;
+
+    public HostNotificationSettingsService(HostNotificationSettingsRepository hostNotificationSettingsRepository,
+                                           UserServiceClient userServiceClient) {
+        this.hostNotificationSettingsRepository = hostNotificationSettingsRepository;
+        this.userServiceClient = userServiceClient;
+    }
+
+    //metodu koristi UserService
+    public MessageResponse createHostNotificationSettings(String jwtToken) {
+        UserDTO userDetails = userServiceClient.getUserDetails(jwtToken);
+        if (userDetails == null) {
+            throw new IllegalStateException("User details could not be retrieved.");
+        }
+        if (!userDetails.getRoles().contains("ROLE_HOST")) {
+            throw new SecurityException("User do not have permission for this action.");
+        }
+
+        if (!hostNotificationSettingsRepository.existsByHost(userDetails.getUsername())) {
+            HostNotificationSettings newHostNotificationSettings = new HostNotificationSettings(
+                    userDetails.getUsername(),
+                    true,
+                    true,
+                    true,
+                    true
+            );
+
+            hostNotificationSettingsRepository.save(newHostNotificationSettings);
+
+            return new MessageResponse("Host notification settings created successfully.");
+        } else {
+            throw new IllegalArgumentException("Host notification settings already exist.");
+        }
+    }
+
+    public HostNotificationSettingsDTO getMyHostNotificationSettings(String jwtToken) {
+        UserDTO userDetails = userServiceClient.getUserDetails(jwtToken);
+        if (userDetails == null) {
+            throw new IllegalStateException("User details could not be retrieved.");
+        }
+        if (!userDetails.getRoles().contains("ROLE_HOST")) {
+            throw new SecurityException("User do not have permission for this action.");
+        }
+
+        HostNotificationSettings hostNotificationSettings = hostNotificationSettingsRepository
+                .findByHost(userDetails.getUsername());
+
+        return HostNotificationSettingsMapper.toHostNotificationSettingsDTO(hostNotificationSettings);
+    }
+
+    public MessageResponse updateMyHostNotificationSettings(
+            UpdateHostNotificationSettingsRequest updateHostNotificationSettingsRequest,
+            String jwtToken) {
+        UserDTO userDetails = userServiceClient.getUserDetails(jwtToken);
+        if (userDetails == null) {
+            throw new IllegalStateException("User details could not be retrieved.");
+        }
+        if (!userDetails.getRoles().contains("ROLE_HOST")) {
+            throw new SecurityException("User do not have permission for this action.");
+        }
+
+        HostNotificationSettings hostNotificationSettings = hostNotificationSettingsRepository
+                .findByHost(userDetails.getUsername());
+
+        hostNotificationSettings.setIsReservationRequestEnabled(
+                updateHostNotificationSettingsRequest.getIsReservationRequestEnabled());
+        hostNotificationSettings.setIsReservationCanceledEnabled(
+                updateHostNotificationSettingsRequest.getIsReservationCanceledEnabled());
+        hostNotificationSettings.setIsHostRatedEnabled(
+                updateHostNotificationSettingsRequest.getIsHostRatedEnabled());
+        hostNotificationSettings.setIsAccommodationRatedEnabled(
+                updateHostNotificationSettingsRequest.getIsAccommodationRatedEnabled());
+
+        hostNotificationSettingsRepository.save(hostNotificationSettings);
+
+        return new MessageResponse("Host notification settings updated successfully.");
+    }
+}
