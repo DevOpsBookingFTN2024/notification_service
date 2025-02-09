44b7f01331798e75f36304ff4da96d7b8822ab4c
Branislav Roljic <branislavroljic99@gmail.com>
2025-02-09 22:30:25 +0100

    fix: remove hardcoded URL from Web Socket's origins

--- src/main/java/uns/ac/rs/notification_service/config/WebSocketConfig.java
+++ src/main/java/uns/ac/rs/notification_service/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package uns.ac.rs.notification_service.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,11 +10,15 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Value("${websocket.allowed-origin}")
+    private String allowedOrigin;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
                 .addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
